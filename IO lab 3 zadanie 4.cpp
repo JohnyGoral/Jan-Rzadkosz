226117d6bd2ae04eecfa9f2fb42a190e91a3c105
JohnyGoral <147827293+JohnyGoral@users.noreply.github.com>
2023-10-27 13:24:10 +0200

    Add files via upload

---
+++ IO lab 3 zadanie 4.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int AA[10]; 
+    int ujemne_count = 0; 
+    int dodatnie_count = 0; 
+
+    for (int i = 0; i < 10; i++) {
+        cout << "Podaj " << i + 1 << ". liczbe: ";
+        cin >> AA[i];
+
+        if (AA[i] < 0) {
+            ujemne_count++; 
+        }
+        else if (AA[i] > 0) {
+            dodatnie_count++; 
+        }
+    }
+
+    vector<int> BB;
+    for (int i = 0; i < 10; i++) {
+        if (AA[i] < 0) {
+            BB.push_back(AA[i]);
+        }
+    }
+
+    cout << "ujemne: " << ujemne_count << endl;
+    cout << "dodatnie: " << dodatnie_count << endl;
+
+    cout << "Liczby ujemne w tablicy BB: ";
+    for (int i = 0; i < BB.size(); i++) {
+        cout << BB[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ Zadanie IO lab 3.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Produkt {
+public:
+    string nazwa;
+    double cena;
+
+    Produkt() {
+        nazwa = "";
+        cena = 0.0;
+    }
+};
+
+
+bool porownajProdukty(const Produkt& produkt1, const Produkt& produkt2) {
+    return produkt1.cena > produkt2.cena;
+}
+
+int main() {
+    vector<Produkt> produkty(10); 
+   
+    for (int i = 0; i < 10; ++i) {
+        cout << "Podaj nazwe produktu " << i + 1 << ": ";
+        cin >> produkty[i].nazwa;
+        cout << "Podaj cene produktu " << i + 1 << ": ";
+        cin >> produkty[i].cena;
+    }
+
+    
+    sort(produkty.begin(), produkty.end(), porownajProdukty);
+
+   
+    cout << "Produkty od najdroższego do najtanszego:\n";
+    for (const Produkt& produkt : produkty) {
+        cout << "Nazwa: " << produkt.nazwa << ", Cena: " << produkt.cena << endl;
+    }
+
+    return 0;
+}
